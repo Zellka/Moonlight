3a04bad9b04e74be1e42b43f05a29b1180b2b27b
Zellka <ilona.zelka@gmail.com>
2021-01-08 19:50:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/moonlightapp/ui/menuapp/BasketFragment.kt
@@ -0,0 +1,20 @@
+package com.example.moonlightapp.ui.menuapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.moonlightapp.R
+
+class BasketFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_basket, container, false)
+        return root
+    }
+}
---
+++ app/src/main/java/com/example/moonlightapp/ui/menuapp/ProfileFragment.kt
@@ -0,0 +1,20 @@
+package com.example.moonlightapp.ui.menuapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.moonlightapp.R
+
+class ProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_profile, container, false)
+        return root
+    }
+}
