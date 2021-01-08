72a329ab2690cd3285c0363bb5a7930c19f0f96e
Zellka <ilona.zelka@gmail.com>
2021-01-08 20:32:43 +0300

    ...

---
+++ app/src/main/java/com/example/moonlightapp/ui/items/DishItem.kt
@@ -0,0 +1,28 @@
+package com.example.moonlightapp.ui.items
+
+import com.example.moonlightapp.R
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
+import com.xwray.groupie.kotlinandroidextensions.Item
+import kotlinx.android.synthetic.main.item_dish.*
+
+class DishItem(private val content: DishContent) : Item() {
+
+    override fun getLayout() = R.layout.item_dish
+
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+        viewHolder.name_dish.text = content.name
+        viewHolder.price_dish.text = content.price
+        viewHolder.description_dish.text = content.description
+        Picasso.get()
+            .load(content.url)
+            .into(viewHolder.img_dish)
+    }
+}
+
+data class DishContent(
+    val name: String,
+    val price: String,
+    val description: String,
+    val url: String
+)
--- app/src/main/java/com/example/moonlightapp/ui/menuapp/MenuFragment.kt
+++ app/src/main/java/com/example/moonlightapp/ui/menuapp/MenuFragment.kt
@@ -6,15 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.moonlightapp.R
+import com.example.moonlightapp.ui.items.DishContent
+import com.example.moonlightapp.ui.items.DishItem
+import com.example.moonlightapp.ui.items.MainContainer
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
+import com.xwray.groupie.kotlinandroidextensions.Item
+import kotlinx.android.synthetic.main.fragment_menu.*
 
 class MenuFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,  savedInstanceState: Bundle?): View? {
+
         val root = inflater.inflate(R.layout.fragment_menu, container, false)
+       /* val dishes = listOf(
+            getAppetizer(),
+        )
+        items_container.adapter = GroupAdapter<GroupieViewHolder>().apply { addAll(dishes) }*/
         return root
     }
+    //private fun getAppetizer(): Item { }
 }
