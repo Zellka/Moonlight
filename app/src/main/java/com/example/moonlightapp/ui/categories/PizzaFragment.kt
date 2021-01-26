f4c92b212e00db87cfa715f8302c5d794d1e1a07
Zellka <ilona.zelka@gmail.com>
2021-01-26 22:04:50 +0300

    Added method onCreateView(...)

--- app/src/main/java/com/example/moonlightapp/ui/categories/PizzaFragment.kt
+++ app/src/main/java/com/example/moonlightapp/ui/categories/PizzaFragment.kt
@@ -1,6 +1,20 @@
 package com.example.moonlightapp.ui.categories
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.moonlightapp.R
 
 class PizzaFragment : Fragment()  {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.dish_list, container, false)
+        return root
+    }
 }
