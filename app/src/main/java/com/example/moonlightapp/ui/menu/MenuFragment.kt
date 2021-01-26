99a8056279d6aa99b5e16dfe2b659d8e22d56484
Zellka <ilona.zelka@gmail.com>
2021-01-26 20:08:56 +0300

    Edit

--- app/src/main/java/com/example/moonlightapp/ui/menu/MenuFragment.kt
+++ app/src/main/java/com/example/moonlightapp/ui/menu/MenuFragment.kt
@@ -5,13 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
 import com.example.moonlightapp.R
+import com.example.moonlightapp.ui.categories.CategoryAdapter
+import com.google.android.material.tabs.TabLayout
+
 
 class MenuFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,  savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
         val root = inflater.inflate(R.layout.fragment_menu, container, false)
+        val adapter = CategoryAdapter(this.requireContext(), childFragmentManager)
+        val viewPager: ViewPager = root.findViewById(R.id.view_pager)
+        viewPager.adapter = adapter
+        val tabs: TabLayout = root.findViewById(R.id.tab_layout)
+        tabs.setupWithViewPager(viewPager)
         return root
     }
 }
