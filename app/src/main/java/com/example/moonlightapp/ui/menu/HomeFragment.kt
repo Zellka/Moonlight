f649a1645654fc432287ada1ad8c4c65f8bc75a7
Zellka <ilona.zelka@gmail.com>
2021-02-03 22:39:10 +0300

    Added ViewFlipper

--- app/src/main/java/com/example/moonlightapp/ui/menu/HomeFragment.kt
+++ app/src/main/java/com/example/moonlightapp/ui/menu/HomeFragment.kt
@@ -1,13 +1,20 @@
 package com.example.moonlightapp.ui.menu
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.ViewFlipper
 import androidx.fragment.app.Fragment
 import com.example.moonlightapp.R
 
 class HomeFragment : Fragment() {
+
+    private lateinit var viewFlipper:ViewFlipper
+    private var images = intArrayOf(R.drawable.food1, R.drawable.food2, R.drawable.food3)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,6 +22,18 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
+        viewFlipper = root.findViewById<ViewFlipper>(R.id.view_flipper)
+        flipImage()
+
         return root
+    }
+    private fun flipImage(){
+        for(i in images) {
+            viewFlipper.setInAnimation(this.context, android.R.anim.slide_in_left)
+            viewFlipper.setOutAnimation(this.context, android.R.anim.slide_out_right)
+            val view = ImageView(this.context)
+            view.setImageResource(i)
+            viewFlipper.addView(view)
+        }
     }
 }
