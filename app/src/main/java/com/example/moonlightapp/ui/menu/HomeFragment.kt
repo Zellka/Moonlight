af4e4ef6bdc88976be67863e0434ce09dcbcdc46
Zellka <ilona.zelka@gmail.com>
2021-02-03 23:45:04 +0300

    Edited

--- app/src/main/java/com/example/moonlightapp/ui/menu/HomeFragment.kt
+++ app/src/main/java/com/example/moonlightapp/ui/menu/HomeFragment.kt
@@ -1,17 +1,20 @@
 package com.example.moonlightapp.ui.menu
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.ViewFlipper
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.replace
 import com.example.moonlightapp.R
+import kotlinx.android.synthetic.main.fragment_home.view.*
 
-class HomeFragment : Fragment() {
+
+class HomeFragment : Fragment(), View.OnClickListener  {
 
     private lateinit var viewFlipper:ViewFlipper
     private var images = intArrayOf(R.drawable.food1, R.drawable.food2, R.drawable.food3)
@@ -22,6 +25,8 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
+       //val imgBtn = root.findViewById<ImageView>(R.id.imgg)
+       // imgBtn.setOnClickListener(this)*/
         viewFlipper = root.findViewById<ViewFlipper>(R.id.view_flipper)
         flipImage()
 
@@ -35,5 +40,19 @@ class HomeFragment : Fragment() {
             view.setImageResource(i)
             viewFlipper.addView(view)
         }
+    }
+
+    override fun onClick(v: View?) {
+        Log.d("Click","image")
+        /*var fragment: Fragment? = null
+        val transaction: FragmentTransaction = requireFragmentManager().beginTransaction()
+        when (requireView().id) {
+            R.id.imgg -> {
+                fragment = MenuFragment()
+                transaction.replace(R.id.content, fragment)
+                transaction.addToBackStack(null)
+                transaction.commit()
+            }
+        }*/
     }
 }
