40a2f9aaccfe2508c43f61601b79c3973f770820
Zellka <ilona.zelka@gmail.com>
2021-02-07 01:40:43 +0300

    Added removeDish

--- app/src/main/java/com/example/moonlightapp/adapter/ShoppingCartAdapter.kt
+++ app/src/main/java/com/example/moonlightapp/adapter/ShoppingCartAdapter.kt
@@ -11,10 +11,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.moonlightapp.R
 import com.example.moonlightapp.cart.CartItem
 import com.example.moonlightapp.cart.ShoppingCart
+import com.example.moonlightapp.util.Removable
 import com.squareup.picasso.Picasso
 
-class ShoppingCartAdapter(var context: Context, var cartItems: MutableList<CartItem>) :
+class ShoppingCartAdapter(var context: Context, var cartItems: MutableList<CartItem>, var removable: Removable) :
     RecyclerView.Adapter<ShoppingCartAdapter.ViewHolder>() {
+
+
+    //private lateinit var removable: Removable
 
     override fun onCreateViewHolder(parent: ViewGroup, p1: Int): ShoppingCartAdapter.ViewHolder {
         val layout = LayoutInflater.from(context).inflate(R.layout.cart_item, parent, false)
@@ -25,34 +29,27 @@ class ShoppingCartAdapter(var context: Context, var cartItems: MutableList<CartI
 
     override fun onBindViewHolder(viewHolder: ShoppingCartAdapter.ViewHolder, position: Int) {
         //viewHolder.bindItem(cartItems[position])
-        viewHolder.nameDish!!.text = cartItems[position].product.name
-        viewHolder.priceDish!!.text = cartItems[position].product.price.toString()
-        viewHolder.quantity!!.text = cartItems[position].quantity.toString()
+        val model = cartItems[position]
+        viewHolder.nameDish!!.text = model.product.name
+        viewHolder.priceDish!!.text = model.product.price.toString()
+        viewHolder.quantity!!.text = model.quantity.toString()
         Picasso.get()
-            .load(cartItems[position].product.url)
+            .load(model.product.url)
             .into(viewHolder.imgDish)
-        Picasso.get().load(cartItems[position].product.url).fit().into(viewHolder.imgDish)
         viewHolder.remove?.setOnClickListener {
-            ShoppingCart.removeItem(cartItems, position)
+            //ShoppingCart.removeItem(cartItems, position)
+            removable.removeDish(cartItems, position)
             notifyDataSetChanged()
         }
     }
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-        var imgDish: ImageView? = null
-        var nameDish: TextView? = null
-        var priceDish: TextView? = null
-        var remove: Button? = null
-        var quantity: TextView? = null
-
-        init {
-            imgDish = itemView.findViewById(R.id.product_image)
-            nameDish = itemView.findViewById(R.id.product_name)
-            priceDish = itemView.findViewById(R.id.product_price)
-            remove = itemView.findViewById(R.id.removeItem)
-            quantity = itemView.findViewById(R.id.product_quantity)
-        }
+        var imgDish: ImageView? = itemView.findViewById(R.id.product_image)
+        var nameDish: TextView? = itemView.findViewById(R.id.product_name)
+        var priceDish: TextView? = itemView.findViewById(R.id.product_price)
+        var remove: Button? = itemView.findViewById(R.id.removeItem)
+        var quantity: TextView? = itemView.findViewById(R.id.product_quantity)
         /*@SuppressLint("SetTextI18n", "CheckResult")
         fun bindItem(cartItem: CartItem) {
 
--- app/src/main/java/com/example/moonlightapp/ui/menu/CartFragment.kt
+++ app/src/main/java/com/example/moonlightapp/ui/menu/CartFragment.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.moonlightapp.R
 import com.example.moonlightapp.adapter.ShoppingCartAdapter
+import com.example.moonlightapp.cart.CartItem
 import com.example.moonlightapp.cart.ShoppingCart
+import com.example.moonlightapp.util.Removable
 
-class CartFragment : Fragment() {
+class CartFragment : Fragment(), Removable {
     lateinit var adapter: ShoppingCartAdapter
     private lateinit var rv: RecyclerView
 
@@ -21,10 +24,15 @@ class CartFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_cart, container, false)
-        adapter = ShoppingCartAdapter(this.requireContext(), ShoppingCart.getCart())
+        adapter = ShoppingCartAdapter(this.requireContext(), ShoppingCart.getCart(), this)
         rv = root.findViewById(R.id.rv_basket)
         rv.adapter = adapter
         rv.layoutManager = LinearLayoutManager(context)
         return root
+    }
+
+    override fun removeDish(cartItems: MutableList<CartItem>, position: Int) {
+        ShoppingCart.removeItem(cartItems, position)
+        Toast.makeText(context, "Удалено", Toast.LENGTH_SHORT).show()
     }
 }
