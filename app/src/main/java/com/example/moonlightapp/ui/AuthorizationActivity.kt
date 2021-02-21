204043b5f5a996788e48cb1c5e826cdc6e5a5252
Zellka <ilona.zelka@gmail.com>
2021-02-21 17:00:28 +0300

    Add MaskedEditText

--- app/src/main/java/com/example/moonlightapp/ui/AuthorizationActivity.kt
+++ app/src/main/java/com/example/moonlightapp/ui/AuthorizationActivity.kt
@@ -2,11 +2,22 @@ package com.example.moonlightapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import com.example.moonlightapp.R
+import com.google.android.material.textfield.TextInputLayout
 
 class AuthorizationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authorization)
+
+        val enterName = findViewById<EditText>(R.id.enter_name)
+        val btnSignUp = findViewById<Button>(R.id.btn_sign_up)
+        val nameError = findViewById<TextInputLayout>(R.id.name_error)
+        val numberError = findViewById<TextInputLayout>(R.id.number_error)
+        val enterNumber = findViewById<EditText>(R.id.enter_number)
+
     }
+
 }
