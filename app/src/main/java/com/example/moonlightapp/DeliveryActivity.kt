60e1a7e554af721b6092bbb61c867ac1cd3b28c1
Zellka <ilona.zelka@gmail.com>
2021-02-21 13:49:16 +0300

    Add DeliveryActivity

---
+++ app/src/main/java/com/example/moonlightapp/DeliveryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.moonlightapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeliveryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delivery)
+    }
+}
