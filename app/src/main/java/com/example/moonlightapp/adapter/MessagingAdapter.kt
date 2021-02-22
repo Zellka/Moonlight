ecc239e9d44e3001acca308d103bfac31fd01d43
Zellka <ilona.zelka@gmail.com>
2021-02-22 14:43:26 +0300

    Add MessagingAdapter for ChatFragment.kt

---
+++ app/src/main/java/com/example/moonlightapp/adapter/MessagingAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.moonlightapp.adapter
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.moonlightapp.utils.Constants.RECEIVE_ID
+import com.example.moonlightapp.utils.Constants.SEND_ID
+import com.example.moonlightapp.R
+import kotlinx.android.synthetic.main.message_item.view.*
+import com.example.moonlightapp.entity.Message
+
+class MessagingAdapter : RecyclerView.Adapter<MessagingAdapter.MessageViewHolder>() {
+
+    var messagesList = mutableListOf<Message>()
+
+    inner class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder =
+        MessageViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.message_item, parent, false)
+        )
+
+    override fun getItemCount(): Int = messagesList.size
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
+        val currentMessage = messagesList[position]
+
+        when (currentMessage.id) {
+            SEND_ID -> {
+                holder.itemView.your_message.apply {
+                    text = currentMessage.message
+                    visibility = View.VISIBLE
+                }
+                holder.itemView.bot_message.visibility = View.GONE
+            }
+            RECEIVE_ID -> {
+                holder.itemView.bot_message.apply {
+                    text = currentMessage.message
+                    visibility = View.VISIBLE
+                }
+                holder.itemView.your_message.visibility = View.GONE
+            }
+        }
+    }
+
+    fun insertMessage(message: Message) {
+        this.messagesList.add(message)
+        notifyItemInserted(messagesList.size)
+    }
+}
