c68e49a2091d70a55bff018d9058312eedae2d19
Zellka <ilona.zelka@gmail.com>
2021-03-17 22:22:12 +0300

    Add setDisplayShowHome

--- app/src/main/java/com/example/moonlightapp/ui/detail/AuthorizationActivity.kt
+++ app/src/main/java/com/example/moonlightapp/ui/detail/AuthorizationActivity.kt
@@ -2,6 +2,7 @@ package com.example.moonlightapp.ui.detail
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -13,16 +14,28 @@ class AuthorizationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_authorization)
         binding.btnSignUp.setOnClickListener {
             if (binding.enterName.text!!.isNotEmpty() && binding.enterNumber.text!!.isNotEmpty()) {
                 val intent = Intent()
                 intent.putExtra("NAME_USER", binding.enterName.text.toString())
                 setResult(RESULT_OK, intent)
-                this.finish();
+                this.finish()
             } else {
                 Toast.makeText(this, "Введите данные", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                this.finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
--- app/src/main/java/com/example/moonlightapp/ui/detail/DeliveryActivity.kt
+++ app/src/main/java/com/example/moonlightapp/ui/detail/DeliveryActivity.kt
@@ -2,11 +2,24 @@ package com.example.moonlightapp.ui.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.example.moonlightapp.R
 
 class DeliveryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_delivery)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                this.finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
