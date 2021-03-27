b4d1e8cfde492a3ae49e4912d889dd051b44c47f
Zellka <ilona.zelka@gmail.com>
2021-03-27 11:52:31 +0300

    Create FavoriteActivity

---
+++ app/src/main/java/com/example/moonlightapp/ui/detail/FavoriteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.moonlightapp.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moonlightapp.R
+
+class FavoriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite)
+    }
+}
