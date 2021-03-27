b3256bb0d5dba1c83f9cbcc53344f4fae2ea9ba4
Zellka <ilona.zelka@gmail.com>
2021-03-27 12:14:06 +0300

    Create FavouriteList.kt

---
+++ app/src/main/java/com/example/moonlightapp/data/FavouriteList.kt
@@ -0,0 +1,32 @@
+package com.example.moonlightapp.data
+
+import com.example.moonlightapp.entity.Dish
+import io.paperdb.Paper
+
+class FavouriteList {
+    companion object {
+        fun updateItem(item: Dish) {
+            val favouriteList = getFavouriteList()
+            val targetItem = favouriteList.singleOrNull { it.name == item.name }
+            if (targetItem == null) {
+                favouriteList.add(item)
+            } else {
+                favouriteList.remove(item)
+            }
+            saveStock(favouriteList)
+        }
+
+        fun isFavourite(item: Dish): Boolean {
+            val favouriteList = getFavouriteList()
+            return favouriteList.contains(item)
+        }
+
+        private fun saveStock(stock: MutableList<Dish>) {
+            Paper.book().write("favourites", stock)
+        }
+
+        fun getFavouriteList(): MutableList<Dish> {
+            return Paper.book().read("favourites", mutableListOf())
+        }
+    }
+}
