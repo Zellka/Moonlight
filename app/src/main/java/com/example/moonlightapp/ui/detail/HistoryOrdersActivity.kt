106f570e553c33ae53f07eece90a593e3225f2e0
Zellka <ilona.zelka@gmail.com>
2021-03-27 17:33:01 +0300

    Create HistoryOrdersActivity

---
+++ app/src/main/java/com/example/moonlightapp/ui/detail/HistoryOrdersActivity.kt
@@ -0,0 +1,25 @@
+package com.example.moonlightapp.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import com.example.moonlightapp.R
+
+class HistoryOrdersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history_orders)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                this.finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
--- app/src/main/java/com/example/moonlightapp/ui/detail/OrderingActivity.kt
+++ app/src/main/java/com/example/moonlightapp/ui/detail/OrderingActivity.kt
@@ -1,16 +1,25 @@
 package com.example.moonlightapp.ui.detail
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import com.example.moonlightapp.R
+import kotlinx.android.synthetic.main.activity_ordering.*
 
-class OrderActivity : AppCompatActivity() {
+class OrderingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_order)
+        setContentView(R.layout.activity_ordering)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setDisplayShowHomeEnabled(true)
+        val intentHistory = Intent(this, HistoryOrdersActivity::class.java)
+        button_order.setOnClickListener {
+            if (userName.text!!.isNotEmpty() && userNumber.text!!.isNotEmpty() && userHome.text!!.isNotEmpty() && userStreet.text!!.isNotEmpty()) {
+                startActivity(intentHistory)
+            }
+        }
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
