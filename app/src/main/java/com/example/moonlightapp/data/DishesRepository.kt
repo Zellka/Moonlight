93e3f3fb7c99a752daff1dbcc5ecd9bf8e3dacad
Zellka <ilona.zelka@gmail.com>
2021-03-29 23:18:27 +0300

    Update DishesRepository.kt

--- app/src/main/java/com/example/moonlightapp/data/DishesRepository.kt
+++ app/src/main/java/com/example/moonlightapp/data/DishesRepository.kt
@@ -9,35 +9,35 @@ class DishesRepository {
             Dish(
                 "Снеки1",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         items.add(
             Dish(
                 "Снеки2",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         items.add(
             Dish(
                 "Снеки3",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         items.add(
             Dish(
                 "Снеки4",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         items.add(
             Dish(
                 "Снеки5",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         return items
@@ -49,35 +49,35 @@ class DishesRepository {
             Dish(
                 "Крабовый1",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         items.add(
             Dish(
                 "Крабовый2",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         items.add(
             Dish(
                 "Крабовый3",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         items.add(
             Dish(
                 "Крабовый4",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         items.add(
             Dish(
                 "Крабовый5",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         return items
@@ -89,35 +89,35 @@ class DishesRepository {
             Dish(
                 "Мясо1",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         items.add(
             Dish(
                 "Мясо2",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         items.add(
             Dish(
                 "Мясо3",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         items.add(
             Dish(
                 "Мясо4",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         items.add(
             Dish(
                 "Мясо5",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         return items
@@ -129,35 +129,35 @@ class DishesRepository {
             Dish(
                 "Гречка1",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         items.add(
             Dish(
                 "Гречка2",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         items.add(
             Dish(
                 "Гречка3",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         items.add(
             Dish(
                 "Гречка4",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         items.add(
             Dish(
                 "Гречка5",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         return items
@@ -169,35 +169,35 @@ class DishesRepository {
             Dish(
                 "Торт1",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         items.add(
             Dish(
                 "Торт2",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         items.add(
             Dish(
                 "Торт3",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         items.add(
             Dish(
                 "Торт4",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         items.add(
             Dish(
                 "Торт5",
                 "289",
-                "https://sun9-29.userapi.com/impg/c854024/v854024546/2235f1/yHBkXIcQnBk.jpg?size=520x0&quality=90&sign=8c559cc05bf42491b56129418a7d488e"
+                "https://4.downloader.disk.yandex.ua/disk/3f570f78654aa47b3b1309dc60c9a06aa2127ff3967ffc054154dd0328295e20/60624143/pvVNPdxkXXJ88uIpOLiGcNzVEl97Uz7CUgNQWKR6fQ5dhvWXBTW8yRUhBo0ZJM2tZVqBSTSmsjOXs-iMHRP_tg%3D%3D?uid=1186320270&filename=pic.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1186320270&fsize=264320&hid=a9e74517c6de8b6dd0066f41ae7486b8&media_type=image&tknv=v2&etag=38fd415aabc79bfdde2e1241bfa44987"
             )
         )
         return items
